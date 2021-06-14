a9601359744eda446754733294f91a4d2f26d699
Mengsreang Chhoeung <mengsreangchhoeung789@gmail.com>
2021-06-14 21:30:57 +0700

    add to github

---
+++ src/main/kotlin/com/webapp/springbootkotlintutorial/api/SpringbootKotlinTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.webapp.springbootkotlintutorial.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinTutorialApplication>(*args)
+}
---
+++ src/main/kotlin/com/webapp/springbootkotlintutorial/api/TestController.kt
@@ -0,0 +1,16 @@
+package com.webapp.springbootkotlintutorial.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("testing-page")
+class TestController {
+
+    @GetMapping
+    fun testingPage(): String {
+        return "<h1>Hello Testing Page😋</h1>"
+    }
+
+}
